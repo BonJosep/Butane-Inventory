ad14944a6bf57ba7ad825ca22ee3555a2fa24e8e
BonJosep <c23-2226-383@uphsl.edu.ph>
2025-12-10 04:22:48 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,49 @@
-const CACHE_NAME = 'butane-pwa-v1';
+const CACHE_NAME = 'butane-pwa-v2';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/service-worker.js'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './service-worker.js'
 ];
 
 self.addEventListener('install', (evt) => {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
   evt.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map(key => { if(key !== CACHE_NAME) return caches.delete(key); })
-    ))
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   if (evt.request.method !== 'GET') return;
+
   evt.respondWith(
-    caches.match(evt.request).then((resp) => {
-      return resp || fetch(evt.request).then((res) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          try { cache.put(evt.request, res.clone()); } catch(e){}
-          return res;
-        })
-      }).catch(()=>caches.match('/'));
+    caches.match(evt.request).then((cached) => {
+      return (
+        cached ||
+        fetch(evt.request)
+          .then((resp) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(evt.request, resp.clone());
+              return resp;
+            });
+          })
+          .catch(() => caches.match('./index.html'))
+      );
     })
   );
 });
